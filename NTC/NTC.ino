9e5a2503ee772106ca6d5c01de7533a33c98fe1f
Tec-Doc <66914582+Tec-Doc@users.noreply.github.com>
2020-06-14 18:16:29 +0200

    Add files via upload
    Copyright hinzugefügt.....

--- NTC/NTC.ino
+++ NTC/NTC.ino
@@ -18,7 +18,31 @@
     (C) 2020 14th of June   Dr.sc. Klaus Helmgens, M.I.T., USA
     This example code is in the public domain an can be used free as long as the
     usage is non comercial.
- *****************************************************************************************/
+ *****************************************************************************************
+
+
+MIT License
+
+Copyright (c) 2020 Tec-Doc
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 
 const float Tr = 298.15;            // Referenz Temperature in Kelvin (25°C)
